e086248dafafb4f054e7c2efcc4abd784cbb0dee
Terrel Shumway <moan-o-storm-beware-spoke@shumway.us>
2016-08-23 05:37:13 -0600

    bump version number. fix description and classifiers

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
 from setuptools import setup, find_packages
 
+def slurp(fname):
+    with open(fname,"rt") as fi:
+        return fi.read()
+
 setup(
     name='correct-horse',
-    version='0.1.0',
+    version='0.1.3',
     description='Print strong, memorable passphrases',
     url='https://github.com/bbiw/correct-horse',
     author='Terrel Shumway',
-    long_description='''This program allows you to easily create strong, memorable passphrases.
-
-    It reads a specified number of bits of data from your system's CSPRNG, turrns it
-    into a very large number, and prints it out in base 7776.
-
-    The result will be something like `greet say yaw dunham italy pass navel`. If
-    your system has a good random number generator, this should be a reasonably secure
-    password, and also reasonably easy to memorize.
-
-    see also:
-        http://world.std.com/~reinhold/diceware.html
-        https://www.rempe.us/diceware/#diceware
-        https://xkcd.com/936/
-        https://github.com/bbiw/mempass
-
-    ''',
+    long_description=slurp("description.rst"),
     classifiers=[
         'Environment :: Console',
         'Development Status :: 3 - Alpha',
@@ -29,8 +18,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Security :: Passwords',
+        'Topic :: Security',
     ],
     author_email='algae-ester-1925-riga-seth@shumway.us',
     license='MIT',
